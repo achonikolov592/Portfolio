12614aa7e617fbc1c8c065ab74ee010272bad9b1
achonikolov592 <achonikolov2005@gmail.com>
2024-10-08 18:13:34 +0200

    Fixed db

--- utils/db.ts
+++ utils/db.ts
@@ -4,11 +4,11 @@ let conn: Pool | null = null;
 
 if (!conn) {
   conn = new Pool({
-    user: process.env.NEXT_PUBLIC_PGSQL_USER,
-    password: process.env.NEXT_PUBLIC_PGSQL_PASSWORD,
-    host: process.env.NEXT_PUBLIC_PGSQL_HOST,
-    port: parseInt(<string>process.env.NEXT_PUBLIC_PGSQL_PORT, 10) || 5432,
-    database: process.env.NEXT_PUBLIC_PGSQL_DATABASE,
+    user: process.env.NEXT_PUBLIC_POSTGRES_USER,
+    password: process.env.NEXT_PUBLIC_POSTGRES_PASSWORD,
+    host: process.env.NEXT_PUBLIC_POSTGRES_HOST,
+    port: parseInt(<string>process.env.NEXT_PUBLIC_POSTGRES_PORT, 10) || 5432,
+    database: process.env.NEXT_PUBLIC_POSTGRES_DATABASE,
   });
 }
 
