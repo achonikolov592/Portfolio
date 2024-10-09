c7630482890baa426bbec0768b227d062c42b29a
achonikolov592 <achonikolov2005@gmail.com>
2024-10-09 19:11:14 +0200

    fixed isr 1.10

--- components/ImageLanguage.tsx
+++ components/ImageLanguage.tsx
@@ -1,44 +1,40 @@
-import {pinata} from "@/utils/pinata"
-import getRows from "@/utils/getRows";
+import { pinata } from "@/utils/pinata"; // Import Pinata utilities
+import getRows from "@/utils/getRows"; // Import your database access function
 
-const ImageLanguage = async () =>{
+const ImageLanguage = async () => {
+    // Fetch data from the database directly
     const data = await getRows("Languages");
 
-    if (data){
-        
-        
-        const imageSrc = await Promise.all(
-            data.map(async (item) => {
-                const url = await pinata.gateways.createSignedURL({
-                    cid: item.cid,
-                    expires: 30,
-                }).optimizeImage({
-                    width: 150,
-                    height: 150,
-                });
-                return url;
-            })
-        );
-
-        return (
-            <div className="inline-block mt-10">
-                <div className="grid grid-cols-3 gap-4">
-                    {imageSrc.map((value, index) => (
-                        <img key={index} src={value} alt="image" />
-                    ))}
-                </div>
-            </div>
-        );
-
-    }else{
-        console.error("No response from the db")
-        return (
-            <div className="inline-block mt-10">
-                No languages to show.
-            </div>
-        );
+    // Check if data is available
+    if (!data) {
+        console.error("No response from the database");
+        return <div>No data available</div>; // Handle the case of no data
     }
-    
-}
 
-export default  ImageLanguage;
+    // Map through the data to create signed URLs
+    const imageSrc = await Promise.all(
+        data.map(async (item) => {
+            const url = await pinata.gateways.createSignedURL({
+                cid: item.cid,
+                expires: 30,
+            }).optimizeImage({
+                width: 150,
+                height: 150,
+            });
+            return url; // Return the generated URL
+        })
+    );
+
+    // Return the JSX to render images
+    return (
+        <div className="inline-block mt-10">
+            <div className="grid grid-cols-3 gap-4">
+                {imageSrc.map((value, index) => (
+                    <img key={index} src={value} alt="image" />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default ImageLanguage;
