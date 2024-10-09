0f659b9640391f0a5bb7747b673b1b628621e10f
achonikolov592 <achonikolov2005@gmail.com>
2024-10-09 19:04:20 +0200

    fixed isr 1.7

--- components/ImageLanguage.tsx
+++ components/ImageLanguage.tsx
@@ -4,59 +4,42 @@ import {pinata} from "@/utils/pinata"
 import getRows from "@/utils/getRows";
 
 const ImageLanguage = async () =>{
-    //const [imageSrc, setImageSrc] = useState<string[]>([]);
+    const data = await getRows("Languages");
 
-    //let imageSrc: string[] = []
-    //useEffect(()=>{
-            //const getIDs = async() =>{
-                const data = await getRows("Languages");
+    if (data){
+        
+        
+        const imageSrc = await Promise.all(
+            data.map(async (item) => {
+                const url = await pinata.gateways.createSignedURL({
+                    cid: item.cid,
+                    expires: 30,
+                }).optimizeImage({
+                    width: 150,
+                    height: 150,
+                });
+                return url;
+            })
+        );
 
-                if (data){
-                    
-                    /*let arrOfSRCs: Array<string> = []
+        return (
+            <div className="inline-block mt-10">
+                <div className="grid grid-cols-3 gap-4">
+                    {imageSrc.map((value, index) => (
+                        <img key={index} src={value} alt="image" />
+                    ))}
+                </div>
+            </div>
+        );
 
-                    for (let i = 0; i < data.length; i++){
-                        const ids = await pinata.gateways.createSignedURL({
-                            cid: data[i].cid,
-                            expires: 30,
-                            }).optimizeImage({
-                                width:150,
-                                height:150
-                            })
-                        arrOfSRCs = [...arrOfSRCs, ids]
-                    }
-                    imageSrc = arrOfSRCs*/
-                    const imageSrc = await Promise.all(
-                        data.map(async (item) => {
-                            const url = await pinata.gateways.createSignedURL({
-                                cid: item.cid,
-                                expires: 30,
-                            }).optimizeImage({
-                                width: 150,
-                                height: 150,
-                            });
-                            return url; // Return the generated URL
-                        })
-                    );
-
-                    return (
-                        <div className="inline-block mt-10">
-                            <div className="grid grid-cols-3 gap-4">
-                                {imageSrc.map((value, index) => (
-                                    <img key={index} src={value} alt="image" />
-                                ))}
-                            </div>
-                        </div>
-                    );
-
-                }else{
-                    console.error("No response from the db")
-                }
-           // } 
-
-            //await getIDs();
-        //}
-        //, []);
+    }else{
+        console.error("No response from the db")
+        return (
+            <div className="inline-block mt-10">
+                No languages to show.
+            </div>
+        );
+    }
     
 }
 
