74b9275e08eb2e837ed9ac4eb25bcc12b6c66863
achonikolov592 <achonikolov2005@gmail.com>
2024-10-09 18:10:40 +0200

    fixed isr 1.1

--- utils/getRows.ts
+++ utils/getRows.ts
@@ -1,7 +1,7 @@
 import conn from "./db";
 
 export default async function getRows(table: string){
-    const info = await conn?.query(`SELECT * FROM "Portfolio"."${table}";`);
+    const info = await conn?.query('SELECT * FROM "Portfolio"."' + table + '";');
 
     if (info){
         return info.rows;
